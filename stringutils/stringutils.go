781f5ac3a83dc2a65d5b2a79fcf87dc3c5fe8fb3
wubin48435 <wubin48435@hundsun.com>
2025-01-19 23:22:24 +0800

    Split

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -81,3 +81,14 @@ func ReplaceStringAtByteIndexBatch(in string, args []string, locs [][]int) strin
 	result = append(result, out[end:]...)
 	return string(result)
 }
+
+// Split is a special case of strings.Split
+// which returns an empty slice if the string is empty
+// borrow from https://github.com/ory/go-convenience/blob/master/stringsx/split.go
+func Split(s, sep string) []string {
+	if s == "" {
+		return []string{}
+	}
+
+	return strings.Split(s, sep)
+}
