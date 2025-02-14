c5209bfb421514c94ac0d03418dd887a2bc5c7e5
武斌 <wubinwill@gmail.com>
2025-02-14 23:33:00 +0800

    Enhance DeepCopy with robust map handling and comprehensive test coverage

--- copier/copier.go
+++ copier/copier.go
@@ -1,11 +1,12 @@
 package copier
 
 import (
+	"reflect"
+
 	"github.com/bytedance/sonic"
 	"github.com/bytedance/sonic/decoder"
 	"github.com/pkg/errors"
 	"golang.org/x/exp/maps"
-	"reflect"
 )
 
 var json = sonic.ConfigDefault
@@ -18,12 +19,31 @@ func DeepCopy(src, target interface{}) error {
 	if reflect.ValueOf(target).Kind() != reflect.Ptr {
 		return errors.New("Target should be a pointer")
 	}
+
 	srcVal := src
-	val, ok := src.(map[string]interface{})
-	if ok {
-		val = maps.Clone(val)
-		srcVal = val
+	srcValue := reflect.ValueOf(src)
+
+	// 获取底层值（如果是指针则获取指针指向的值）
+	for srcValue.Kind() == reflect.Ptr {
+		srcValue = srcValue.Elem()
 	}
+
+	// 如果底层类型是 map，进行深拷贝
+	if srcValue.Kind() == reflect.Map {
+		// 尝试转换为 map[string]interface{}
+		if val, ok := srcValue.Interface().(map[string]interface{}); ok {
+			srcVal = maps.Clone(val)
+		} else {
+			// 对于其他类型的 map，创建一个新的 map 并复制所有键值对
+			newMap := reflect.MakeMap(srcValue.Type())
+			iter := srcValue.MapRange()
+			for iter.Next() {
+				newMap.SetMapIndex(iter.Key(), iter.Value())
+			}
+			srcVal = newMap.Interface()
+		}
+	}
+
 	b, err := json.MarshalToString(srcVal)
 	if err != nil {
 		return errors.WithStack(err)
--- copier/copier_test.go
+++ copier/copier_test.go
@@ -1,8 +1,9 @@
 package copier
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type Family struct {
@@ -30,67 +31,130 @@ type TestStructShadow struct {
 }
 
 func TestDeepCopy(t *testing.T) {
-	pets := make(map[string]string)
-	pets["a"] = "dog"
-	pets["b"] = "cat"
+	t.Run("nil values", func(t *testing.T) {
+		var target interface{}
+		err := DeepCopy(nil, &target)
+		assert.NoError(t, err)
+		assert.Nil(t, target)
+	})
 
-	family := Family{
-		Father: "Jack",
-		Mather: "Lily",
-		Pets:   pets,
-		Toys: []string{
-			"car",
-			"lego",
-		},
-	}
-	src := TestStruct{
-		Name:   "Rose",
-		Age:    18,
-		Family: family,
-	}
+	t.Run("non-pointer target", func(t *testing.T) {
+		var target string
+		err := DeepCopy("source", target)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "Target should be a pointer")
+	})
 
-	var target TestStructShadow
+	t.Run("basic types", func(t *testing.T) {
+		src := "hello"
+		var target string
+		err := DeepCopy(src, &target)
+		assert.NoError(t, err)
+		assert.Equal(t, src, target)
+	})
 
-	type args struct {
-		src    interface{}
-		target interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			args: args{
-				src:    src,
-				target: &target,
+	t.Run("map[string]interface{}", func(t *testing.T) {
+		src := map[string]interface{}{
+			"name": "test",
+			"age":  30,
+			"nested": map[string]interface{}{
+				"key": "value",
 			},
-			wantErr: false,
-		},
-		{
-			args: args{
-				src:    nil,
-				target: nil,
+		}
+		var target map[string]interface{}
+		err := DeepCopy(src, &target)
+		assert.NoError(t, err)
+
+		// 使用 JSONEq 比较内容
+		srcJSON, _ := json.Marshal(src)
+		targetJSON, _ := json.Marshal(target)
+		assert.JSONEq(t, string(srcJSON), string(targetJSON))
+
+		// 验证深拷贝
+		src["name"] = "modified"
+		src["nested"].(map[string]interface{})["key"] = "modified"
+		assert.NotEqual(t, src["name"], target["name"])
+		assert.NotEqual(t, src["nested"].(map[string]interface{})["key"],
+			target["nested"].(map[string]interface{})["key"])
+	})
+
+	t.Run("pointer to map", func(t *testing.T) {
+		src := &map[string]interface{}{
+			"name": "test",
+			"age":  30,
+		}
+		var target map[string]interface{}
+		err := DeepCopy(src, &target)
+		assert.NoError(t, err)
+
+		// 使用 JSONEq 比较内容
+		srcJSON, _ := json.Marshal(*src)
+		targetJSON, _ := json.Marshal(target)
+		assert.JSONEq(t, string(srcJSON), string(targetJSON))
+	})
+
+	t.Run("custom map type", func(t *testing.T) {
+		src := map[int]string{
+			1: "one",
+			2: "two",
+		}
+		var target map[int]string
+		err := DeepCopy(src, &target)
+		assert.NoError(t, err)
+		assert.Equal(t, src, target)
+
+		// 验证深拷贝
+		src[1] = "modified"
+		assert.NotEqual(t, src[1], target[1])
+	})
+
+	t.Run("complex nested structure", func(t *testing.T) {
+		type nested struct {
+			Field string
+			Map   map[string]int
+		}
+		src := nested{
+			Field: "test",
+			Map: map[string]int{
+				"one": 1,
+				"two": 2,
 			},
-			wantErr: false,
-		},
-		{
-			name: "",
-			args: args{
-				src:    make(chan string),
-				target: &target,
-			},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := DeepCopy(tt.args.src, tt.args.target); (err != nil) != tt.wantErr {
-				t.Errorf("DeepCopy() error = %v, wantErr %v", err, tt.wantErr)
-			}
-			fmt.Printf("%#v\n", tt.args.target)
-		})
-	}
+		}
+		var target nested
+		err := DeepCopy(src, &target)
+		assert.NoError(t, err)
+
+		// 使用 JSONEq 比较内容
+		srcJSON, _ := json.Marshal(src)
+		targetJSON, _ := json.Marshal(target)
+		assert.JSONEq(t, string(srcJSON), string(targetJSON))
+
+		// 验证深拷贝
+		src.Map["one"] = 100
+		assert.NotEqual(t, src.Map["one"], target.Map["one"])
+	})
+
+	t.Run("concurrent map access", func(t *testing.T) {
+		src := map[string]interface{}{
+			"data": "value",
+		}
+
+		done := make(chan bool)
+		for i := 0; i < 10000; i++ {
+			go func() {
+				var target map[string]interface{}
+				err := DeepCopy(src, &target)
+				assert.NoError(t, err)
+				assert.Equal(t, "value", target["data"])
+				done <- true
+			}()
+		}
+
+		// 等待所有goroutine完成
+		for i := 0; i < 10000; i++ {
+			<-done
+		}
+	})
 }
 
 func TestDeepCopy_ShouldHasError(t *testing.T) {
